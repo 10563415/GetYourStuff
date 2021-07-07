7e60dec230685395a9eeee7f1d762671d234c72f
samarth <30378006+samarth1989@users.noreply.github.com>
2021-07-07 23:57:43 +0100

    Changed the base template from common to base.html

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,7 @@ from flask_login import LoginManager
 
 bootstrap = Bootstrap()
 mail = Mail()
-#moment = Moment()
+moment = Moment()
 db = SQLAlchemy()
 
 login_manager = LoginManager()
@@ -21,7 +21,7 @@ def create_app(config_name):
 
     bootstrap.init_app(app)
     mail.init_app(app)
-    #moment.init_app(app)
+    moment.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
 
