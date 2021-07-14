da778238fb9232c4d23a60d98b68c8749e9595d5
samarth <30378006+samarth1989@users.noreply.github.com>
2021-07-14 20:33:34 +0100

    added port for heroku failures

--- appstart.py
+++ appstart.py
@@ -3,7 +3,10 @@ from flask_migrate import Migrate
 from app import create_app, db
 from app.models import User, Role
 
+
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+app.listen(process.env.PORT || 3000)
+
 migrate = Migrate(app, db)
 
 @app.shell_context_processor
