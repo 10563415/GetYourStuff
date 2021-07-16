534108104b52c8fd7a9d9bb89b9752c1d36de415
samarth <30378006+samarth1989@users.noreply.github.com>
2021-07-16 20:15:36 +0100

    corrected the config values

--- config.py
+++ config.py
@@ -4,38 +4,18 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     
+    
     SECRET_KEY = os.environ.get('SECRET_KEY') or os.urandom(32)
-    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.gmail.com')
+    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.googlemail.com')
     MAIL_PORT = int(os.environ.get('MAIL_PORT', '587'))
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_SSL', 'True').lower() in ['True', 'on', '1']
-    MAIL_USERNAME = os.environ.get('10563415SA@gmail.com')
-    MAIL_PASSWORD = os.environ.get('dwckdclvrrkmynsj')
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_SSL', 'True') in ['True', 'on', '1']
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME','10563415SA@gmail.com')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD','dwckdclvrrkmynsj')
     APP_MAIL_SUBJECT_PREFIX = '[GetYourStuff]'
-    APP_MAIL_SENDER = '10563415SA@gmail.com'
+    APP_MAIL_SENDER = os.environ.get('APP_MAIL_SENDER','10563415SA@gmail.com')
     APP_ADMIN = os.environ.get('APP_ADMIN') or 'meghana.mohapatra93@gmail.com'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    
-    '''
-    SECRET_KEY = os.environ.get('SECRET_KEY') or os.urandom(32)
-    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.mailtrap.io')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT', '2525'))
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'True').lower() in ['True', 'on', '1']
-    MAIL_USERNAME = os.environ.get('74aacdb5c3ef26')
-    MAIL_PASSWORD = os.environ.get('428978dff56aff')
-    APP_MAIL_SUBJECT_PREFIX = '[GetYourStuff]'
-    APP_MAIL_SENDER = 'samarthrout@gmail.com'
-    APP_ADMIN = os.environ.get('APP_ADMIN') or 'samarthrout@gmail.com'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MAIL_DEFAULT_SENDER= 'samarthrout@gmail.com'
-    
-    
-    app.config['MAIL_SERVER']='smtp.mailtrap.io'
-    app.config['MAIL_PORT'] = 2525
-    app.config['MAIL_USERNAME'] = '74aacdb5c3ef26'
-    app.config['MAIL_PASSWORD'] = '428978dff56aff'
-    app.config['MAIL_USE_TLS'] = True
-    app.config['MAIL_USE_SSL'] = False
-    '''
+
 
     SSL_REDIRECT = False
     @staticmethod
@@ -54,9 +34,9 @@ class TestingConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
         'sqlite://'
 
-
+#Empty string is used as default value to avoid error while debugging 
 class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace('postgres://', 'postgresql://') or \
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL','').replace('postgres://', 'postgresql://') or \
         'sqlite:///' + os.path.join(basedir, 'data.sqlite')
     
     @classmethod
